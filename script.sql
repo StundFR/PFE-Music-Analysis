712e25ed40db3ac66ab478e126efa20b53cdf73a
Armand Verd <amalric110@gmail.com>
2022-11-26 17:37:18 +0100

    script postgre

---
+++ script.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS customers;
+
+
+CREATE TABLE artist(
+   id_artist INT GENERATED ALWAYS AS IDENTITY,
+   nameArtist VARCHAR(100),
+   creationDate DATE,
+   city VARCHAR(100),
+   country VARCHAR(100),
+   PRIMARY KEY(id_artist)
+);
+
+CREATE TABLE music(
+   id_music INT GENERATED ALWAYS AS IDENTITY,
+   id_artist INT,
+   title VARCHAR(100),
+   lyrics VARCHAR(1000),
+   genre VARCHAR(100),
+   releaseDate DATE,
+   ranking INT,
+   PRIMARY KEY(id_music),
+   CONSTRAINT fk_artist
+      FOREIGN KEY(id_artist) 
+	  REFERENCES artist(id_artist)
+	  ON DELETE CASCADE
+);
